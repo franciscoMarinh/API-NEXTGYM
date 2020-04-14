012ed8b384478f969eed618a8c4cd6c61547e235
francisco marinho <franc1sc1566@gmail.com>
2020-04-13 21:47:20 -0300

    WIP: entity models populated and route databaseModel implemented

--- src/api/modules/databasemodel/databaseModel.controller.ts
+++ src/api/modules/databasemodel/databaseModel.controller.ts
@@ -1,16 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 import HttpController from '../../commons/controller/http.controller'
+import { getConnection } from 'typeorm'
 import { User } from '../../../database/entity/User'
 
 class DatabaseModelController extends HttpController {
   public get = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const connection = this.getConnection()
+    const connection = getConnection()
     const users = await connection.manager.find(User)
     this.sendResponse(res, next, users)
   }
 
   public post = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const connection = this.getConnection()
+    const connection = getConnection()
     const { firstName, lastName, age } = req.body
     const user = new User()
     user.firstName = firstName
